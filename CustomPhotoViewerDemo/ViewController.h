6eae6c79291a905451e21f3596f65d08b6db3e39
shuai <chang-shuai@hotmail.com>
2016-03-01 18:17:45 +0800

    first commit

---
+++ CustomPhotoViewerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomPhotoViewerDemo
+//
+//  Created by changshuai on 3/1/16.
+//  Copyright © 2016 changshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# CustomPhotoViewer.git
